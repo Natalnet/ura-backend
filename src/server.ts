411e23b04916db223a1a3b7395d6cd5b9d0b62b0
pedroksty <pedrotrab2132@gmail.com>
2020-06-20 03:31:21 -0300

    Add Jest and Babel

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ Serve: "ON" });
+  return response.json({ Serve: "on" });
 });
 
 app.listen(4444);
